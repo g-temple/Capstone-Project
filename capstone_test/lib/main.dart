3b146160368b9821fd047fb02fd48fe8de991520
James McIntyre <98340552+JamesMcIntyre123@users.noreply.github.com>
2024-02-26 13:21:57 -0500

    changed button text

--- capstone_test/lib/main.dart
+++ capstone_test/lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              '# of times Billini lied:',
+              'Press to raise number:',
             ),
             Text(
               '$_counter',
