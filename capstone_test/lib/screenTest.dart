f4ff0d4f913999b81df7b4389f6cef2717772e3d
James McIntyre <98340552+JamesMcIntyre123@users.noreply.github.com>
2024-03-03 15:42:53 -0500

    Task and Reward Pages

--- capstone_test/lib/screenTest.dart
+++ capstone_test/lib/screenTest.dart
@@ -77,17 +77,188 @@ class LogIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text('LogIn'),
+        ),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.person), hintText: 'Enter Username'),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.key), hintText: 'Enter Password'),
+            ),
+            SizedBox(height: 60),
+            ElevatedButton(
+              child: const Text('Log In'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+            )
+          ],
+        )));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('LogIn'),
+        title: const Text('Home'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go Back'),
+          child: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          const Image(
+            image: AssetImage('images/character.png'),
+            height: 130,
+          ),
+          SizedBox(height: 40),
+          ElevatedButton(
+            child: const Text('TASKS'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const TaskHome()),
+              );
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Rewards'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const RewardsHome()),
+              );
+            },
+          )
+        ],
+      )),
+    );
+  }
+}
+
+class TaskHome extends StatelessWidget {
+  const TaskHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('To Do!'),
         ),
+        body: Center(
+            child: Column(children: <Widget>[
+          DataTable(
+            columns: const <DataColumn>[
+              DataColumn(
+                label: Expanded(
+                  child: Text(
+                    'Task',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
+              ),
+              DataColumn(
+                label: Expanded(
+                  child: Text(
+                    'Do By',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
+              ),
+              DataColumn(
+                label: Expanded(
+                  child: Text(
+                    'Type',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
+              ),
+            ],
+            rows: const <DataRow>[
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('Brush Teeth')),
+                  DataCell(Text('10pm')),
+                  DataCell(Text('Home')),
+                ],
+              ),
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('Math Homework')),
+                  DataCell(Text('8pm')),
+                  DataCell(Text('School')),
+                ],
+              ),
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('Pack Lunch')),
+                  DataCell(Text('8am')),
+                  DataCell(Text('Home')),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            child: const Text('Add Tasks'),
+            onPressed: () {},
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            child: const Text('View Completed Tasks'),
+            onPressed: () {},
+          ),
+        ])));
+  }
+}
+
+class RewardsHome extends StatelessWidget {
+  const RewardsHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rewards'),
       ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          const Image(
+            image: AssetImage('images/game.png'),
+            height: 130,
+          ),
+          SizedBox(height: 40),
+          ElevatedButton(
+            child: const Text('Play Game'),
+            onPressed: () {},
+          ),
+          SizedBox(height: 60),
+          const Image(
+            image: AssetImage('images/character.png'),
+            height: 130,
+          ),
+          ElevatedButton(
+            child: const Text('Customize Character'),
+            onPressed: () {},
+          )
+        ],
+      )),
     );
   }
 }
